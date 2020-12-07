dc1de2a6a85c4e939fdb94fdad6316751fb3fbf8
huybeme <huyftw@aim.com>
2020-12-06 21:21:27 -0500

    second commit completed the searchEntry function created a homepage function with some html updated readme

--- Main.go
+++ Main.go
@@ -4,10 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"log"
 	"net/http"
-	"reflect"
-	"strings"
 )
 
 func CheckErr(prompt string, err error) {
@@ -17,8 +16,15 @@ func CheckErr(prompt string, err error) {
 }
 
 type Entry struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
+	ID                  int     `json:"id"`
+	Title               string  `json:"title"`
+	ReleaseDate         string  `json:"release_date"`
+	RecommendationCount int     `json:"recommendation_count"`
+	Price               float64 `json:"price"`
+	Description         string  `json:"desc"`
+	PCminReq            string  `json:"pc_min_req"`
+	LinuxMinReq         string  `json:"linux_min_req"`
+	MacMinReq           string  `json:"mac+min_req"`
 }
 
 func main() {
@@ -29,14 +35,16 @@ func main() {
 
 	router := mux.NewRouter()
 
-	//router.HandleFunc("/gameapi", getEntries).Methods("GET")
-	router.HandleFunc("/gameapi", getEntries).Methods("GET")
+	router.HandleFunc("/gameapi/all-entries/", getEntries).Methods("GET")
+	router.HandleFunc("/gameapi/single-entry/{id}", getEntry).Methods("GET")
+
+	// functions not created, may be out of the scope of this project since our database source is an excel file
 	//router.HandleFunc("/gameapi", createEntry).Methods("POST")
-	router.HandleFunc("/gameapi/{id}", getEntry).Methods("GET")
 	//router.HandleFunc("/gameapi/{id}", udateEntry).Methods("PUT")
 	//router.HandleFunc("/gameapi/{api}", deleteEntry).Methods("DELETE")
 
-	router.HandleFunc("/gameapi/search/", searchEntries).Methods("GET")
+	router.HandleFunc("/gameapi", getHomepage)
+	router.HandleFunc("/gameapi/query/{search}", searchEntries).Methods("GET")
 
 	port := ":8080"
 	err := http.ListenAndServe(port, router)
@@ -44,40 +52,57 @@ func main() {
 
 }
 
+type Homepage struct {
+	Title string
+	News  string
+}
+
+func getHomepage(w http.ResponseWriter, r *http.Request) {
+	header := Homepage{"Welcome to the GameAPI Homepage", "Perform a search below."}
+	temp, err := template.ParseFiles("./index.html")
+	CheckErr("Template parse files error at getHomepage ", err)
+	err = temp.Execute(w, header)
+	CheckErr("Template execute error at getHomepage", err)
+
+	fmt.Println("Homepage Reached")
+}
+
 func searchEntries(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	result, err := GameDataBase.Query("SELECT id, QueryName FROM GameData")
+	query := mux.Vars(r)
+	result, err := GameDataBase.Query("SELECT " +
+		"id, QueryName, ReleaseDate, RecommendationCount, PriceFinal, AboutText, PCMinReqsText, LinuxMinReqsText, " +
+		"MacMinReqsText FROM GameData WHERE QueryName LIKE '%" + query["search"] + "%'")
 	CheckErr("Query error at searchEntries ", err)
 	defer result.Close()
 
 	var entries []Entry
 	for result.Next() {
 		var entry Entry
-		err = result.Scan(&entry.ID, &entry.Title)
+		err = result.Scan(&entry.ID, &entry.Title, &entry.ReleaseDate, &entry.RecommendationCount, &entry.Price,
+			&entry.Description, &entry.PCminReq, &entry.LinuxMinReq, &entry.MacMinReq)
 		CheckErr("Scan error at getEntries ", err)
-
-		fmt.Println(reflect.TypeOf(entry.Title))
-
-		if strings.Contains(entry.Title, "half") {
-			entries = append(entries, entry)
-		}
+		entries = append(entries, entry)
 	}
-	fmt.Println(entries)
-
+	json.NewEncoder(w).Encode(entries)
+	fmt.Println("Search entry complete")
 }
 
 func getEntries(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var entries []Entry
-	result, err := GameDataBase.Query("SELECT id, QueryName FROM GameData")
+	result, err := GameDataBase.Query("SELECT " +
+		"id, QueryName, ReleaseDate, RecommendationCount, PriceFinal, AboutText, PCMinReqsText, LinuxMinReqsText, " +
+		"MacMinReqsText FROM GameData")
 	CheckErr("Query error at getEntries ", err)
 	defer result.Close()
 
 	for result.Next() {
 		var entry Entry
-		err = result.Scan(&entry.ID, &entry.Title)
+		err = result.Scan(&entry.ID, &entry.Title, &entry.ReleaseDate, &entry.RecommendationCount, &entry.Price,
+			&entry.Description, &entry.PCminReq, &entry.LinuxMinReq, &entry.MacMinReq)
 		CheckErr("Scan error at getEntries ", err)
 		entries = append(entries, entry)
 	}
@@ -89,13 +114,16 @@ func getEntry(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	key := mux.Vars(r)
-	result, err := GameDataBase.Query("SELECT id, QueryName FROM GameData WHERE id = ?", key["id"])
+	result, err := GameDataBase.Query("SELECT "+
+		"id, QueryName, ReleaseDate, RecommendationCount, PriceFinal, AboutText, PCMinReqsText, LinuxMinReqsText, "+
+		"MacMinReqsText FROM GameData WHERE id = ?", key["id"])
 	CheckErr("Query error at getEntry ", err)
 	defer result.Close()
 
 	var entry Entry
 	for result.Next() {
-		err = result.Scan(&entry.ID, &entry.Title)
+		err = result.Scan(&entry.ID, &entry.Title, &entry.ReleaseDate, &entry.RecommendationCount, &entry.Price,
+			&entry.Description, &entry.PCminReq, &entry.LinuxMinReq, &entry.MacMinReq)
 		CheckErr("Scan error at getEntries ", err)
 	}
 	json.NewEncoder(w).Encode(entry)
--- README.txt
+++ README.txt
@@ -1,7 +1,24 @@
-Project 2) Write a an API driven site that you host yourself (you can’t use something like
+**Objective**
+(Project 2)
+Write a an API driven site that you host yourself (you can’t use something like
 rapidapi to host it). Your program needs to read the “games-features.xlsx” file and store all the
 data into a sqlite3 database. (you can set this up as a separate mini-program that you usually
 only run once). Once the data is in the database, design a simple API which will allow users to
 search on a game and will return the game’s database entry as a json response.
 Hint – check out: https://github.com/avelino/awesome-go#microsoft-excel
 
+**Instructions**
+go build the project to start the program. The following pages will do the following in your preferred web browser
+
+http://localhost:8080/gameapi                   -> The homepage, contains a search bar to make an entry - does not work
+http://localhost:8080/gameapi/all-entries/      -> Will get all database entries as a JSON response
+http://localhost:8080/gameapi/single-entry/{id} -> replace {id} with integer to get that key's element
+http://localhost:8080/gameapi/query/{search}    -> replace {search} with a search entry to get any related entries
+
+if the database is empty, in the main.go file, under the main function, uncomment the fillDatabase(GameDataBase) line
+and rebuild the project. Filling in the database will take approx. one hour
+
+
+Name:       Huy Le
+Course:     Comp510
+Email :     hle@student.bridgew.edu
